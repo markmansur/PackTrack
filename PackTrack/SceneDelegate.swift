6f1a2c64babe39c63e1eb216a253c0db16a49122
Mark Mansur <mmansur2@uwo.ca>
2019-08-23 22:38:18 -0400

    setup window programmatically

--- PackTrack/SceneDelegate.swift
+++ PackTrack/SceneDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import SwiftUI
+
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,6 +19,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            let navController = UINavigationController(rootViewController: PackagesController())
+            navController.isNavigationBarHidden = true
+            window.rootViewController = navController
+            self.window = window
+            window.makeKeyAndVisible()
+        }
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
