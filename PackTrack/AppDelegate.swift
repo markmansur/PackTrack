58af41bf5f49134fddbcfe4c9c6fec04bed6b5b7
Mark Mansur <mmansur2@uwo.ca>
2019-09-16 15:45:59 -0400

    update AppDelegate to use EnvironmentVariables enumeration

--- PackTrack/AppDelegate.swift
+++ PackTrack/AppDelegate.swift
@@ -12,16 +12,11 @@ import GoogleMaps
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-//     AIzaSyAPsCmI7Uh7JZPJWnpWx7s4y-xK22Uee4A
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyAPsCmI7Uh7JZPJWnpWx7s4y-xK22Uee4A")
-        
-        
+        if let googleAPIKey = EnvironmentVariables.GOOGLE_API_KEY.value {
+            GMSServices.provideAPIKey(googleAPIKey)
+        }
         return true
     }
-    
-    
 }
-
