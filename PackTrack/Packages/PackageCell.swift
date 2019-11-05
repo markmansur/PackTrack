464fd23ee92aba5d294384a8b135631b95a2fb61
Mark Mansur <mmansur2@uwo.ca>
2019-11-05 13:31:43 -0500

    fix PackageCell statusLabel for AfterShip refactor

--- PackTrack/Packages/PackageCell.swift
+++ PackTrack/Packages/PackageCell.swift
@@ -17,16 +17,16 @@ class PackageCell: UICollectionViewCell {
             setupCarrierImage()
             
             switch package?.status {
-            case "PRE_TRANSIT":
-                statusLabel.text = "Pre transit"
-            case "TRANSIT":
+            case "InfoReceived":
+                statusLabel.text = "Info received"
+            case "InTransit":
                 statusLabel.text = "In transit"
-            case "DELIVERED":
+            case "Delivered":
                 statusLabel.text = "Delivered"
-            case "RETURNED":
-                statusLabel.text = "Returned"
-            case "FAILURE":
-                statusLabel.text = "Failure"
+            case "OutForDelivery":
+                statusLabel.text = "Out for delivery"
+            case "Exception":
+                statusLabel.text = "Exception"
             default:
                 statusLabel.text = "Unknown"
             }
